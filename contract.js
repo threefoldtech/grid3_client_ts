898890a8bc281612f8afd0b874b9a17f75340dbf
ahmedhanafy725 <hanafya@codescalers.com>
2021-08-13 08:51:37 +0200

    Remove cached files

--- contract.js
+++
@@ -1,3 +0,0 @@
-var contract = require('../tfchain/cli-tool/src/contract.js');
-var contract_id = contract.createContract({ n: 4, d: "", h: "319a14eb142f54b2d026a448f2ea8885", p: 0 });
-console.log(contract_id);

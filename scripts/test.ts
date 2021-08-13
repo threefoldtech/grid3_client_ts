cb8dbd1f0d831d0148623208b2c1f5c3f0eec5af
ahmedhanafy725 <hanafya@codescalers.com>
2021-08-13 11:32:49 +0200

    add ssh_key var at the beginning of the scripts

--- scripts/test.ts
+++ scripts/test.ts
@@ -11,8 +11,9 @@ async function main() {
     const twin_id = 10
     const mnemonic = "false boss tape wish talent pool ghost token exhibit response hedgehog invite";
     const url = "wss://explorer.devnet.grid.tf/ws"
-    let node_id = 2;
-    let node_twin_id = 3;
+    const node_id = 2;
+    const node_twin_id = 3;
+    const ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmm8OzLt+lTdGaMUwMFcw0P+vr+a/h/UsR//EzzeQsgNtC0bdls4MawVEhb3hNcycEQNd2P/+tXdLC4qcaJ6iABYip4xqqAeY098owGDYhUKYwmnMyo+NwSgpjZs8taOhMxh5XHRI+Ifr4l/GmzbqExS0KVD21PI+4sdiLspbcnVBlg9Eg9enM///zx6rSkulrca/+MnSYHboC5+y4XLYboArD/gpWy3zwIUyxX/1MjJwPeSnd5LFBIWvPGrm3cl+dAtADwTZRkt5Yuet8y5HI73Q5/NSlCdYXMtlsKBLpJu3Ar8nz1QfSQL7dB8pa7/sf/s8wO17rXqWQgZG6JzvZ root@ahmed-Inspiron-3576"
     const contract_id = 18; // used only in case of updating deployment.
 
     // Create zmount workload
@@ -74,7 +75,7 @@ async function main() {
     zmachine.mounts = [mount];
     zmachine.entrypoint = "/sbin/zinit init";
     zmachine.compute_capacity = compute_capacity;
-    zmachine.env = { "SSH_KEY": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmm8OzLt+lTdGaMUwMFcw0P+vr+a/h/UsR//EzzeQsgNtC0bdls4MawVEhb3hNcycEQNd2P/+tXdLC4qcaJ6iABYip4xqqAeY098owGDYhUKYwmnMyo+NwSgpjZs8taOhMxh5XHRI+Ifr4l/GmzbqExS0KVD21PI+4sdiLspbcnVBlg9Eg9enM///zx6rSkulrca/+MnSYHboC5+y4XLYboArD/gpWy3zwIUyxX/1MjJwPeSnd5LFBIWvPGrm3cl+dAtADwTZRkt5Yuet8y5HI73Q5/NSlCdYXMtlsKBLpJu3Ar8nz1QfSQL7dB8pa7/sf/s8wO17rXqWQgZG6JzvZ root@ahmed-Inspiron-3576" };
+    zmachine.env = { "SSH_KEY": ssh_key };
 
     let zmachine_workload = new Workload();
     zmachine_workload.version = 0;

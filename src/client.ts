4e2b6abc847200794be85e141ec3214fb09ba9e9
dmahmouali <dmahmou213@gmail.com>
2021-11-11 01:31:01 +0200

    add the scheduler to the grid client

--- src/client.ts
+++ src/client.ts
@@ -7,6 +7,7 @@ import { appPath } from "./storage/backend";
 import * as modules from "./modules/index";
 import { BackendStorageType, BackendStorage } from "./storage/backend";
 import { GridClientConfig, NetworkEnv } from "./config";
+import { Scheduler } from "./scheduler";
 
 class GridClient {
     static config: GridClientConfig;
@@ -20,6 +21,7 @@ class GridClient {
     twins: modules.twins;
     kvstore: modules.kvstore;
     twinId: number;
+    scheduler: Scheduler;
 
     constructor(
         public network: NetworkEnv,
@@ -33,6 +35,7 @@ class GridClient {
     async connect() {
         const urls = this.getDefaultUrls(this.network);
         const tfclient = new TFClient(urls.substrate, this.mnemonic, this.storeSecret, this.keypairType);
+        this.scheduler = new Scheduler(urls.rmbProxy);
         await tfclient.connect();
         this.twinId = await tfclient.twins.getMyTwinId();
         this._connect();

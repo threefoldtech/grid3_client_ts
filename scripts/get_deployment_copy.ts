96a45d0e0475c58a315ec8e4eb145c370d004229
ranatrk <rana.t.hassan@gmail.com>
2021-11-18 14:25:17 +0200

    Add nightly run workflow WIP

---
+++ scripts/get_deployment_copy.ts
@@ -0,0 +1,15 @@
+import { HTTPMessageBusClient } from "ts-rmb-http-client";
+
+async function main() {
+    console.log("Trial 2 >>>>>>>>>>>>>>>>>>>>>")
+    const myTwin = 106;
+    const node_twin_id = 15;
+    const cmd = "zos.deployment.get";
+    const payload = JSON.stringify({ contract_id: 799 });
+    let rmb = new HTTPMessageBusClient(myTwin, "https://gridproxy.test.grid.tf");
+    let msg = rmb.prepare(cmd, [node_twin_id], 0, 2);
+    await rmb.send(msg, payload);
+    const result = await rmb.read(msg);
+    console.log(result);
+}
+main();

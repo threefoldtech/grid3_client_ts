45fbcb0f0b02cff04e86b8da9ed112da43aa2cee
ranatrk <rana.t.hassan@gmail.com>
2021-11-22 13:56:33 +0200

    Add kubernetes deployment to nightly workflow
    Update kubernetes deployment

--- scripts/kubernetes.ts
+++ scripts/kubernetes.ts
@@ -6,13 +6,13 @@ import { getClient } from "./client_loader";
 
 // create network Object
 const n = new NetworkModel();
-n.name = "monNetwork";
+n.name = "tuesNetwork2";
 n.ip_range = "10.238.0.0/16";
 
 // create k8s node Object
 const master = new KubernetesNodeModel();
-master.name = "master";
-master.node_id = 4;
+master.name = "master1";
+master.node_id = 16;
 master.cpu = 1;
 master.memory = 1024 * 2;
 master.rootfs_size = 1;
@@ -22,8 +22,8 @@ master.planetary = true;
 
 // create k8s node Object
 const worker = new KubernetesNodeModel();
-worker.name = "worker";
-worker.node_id = 4;
+worker.name = "worker1";
+worker.node_id = 16;
 worker.cpu = 2;
 worker.memory = 1024 * 4;
 worker.rootfs_size = 1;
@@ -33,7 +33,7 @@ worker.planetary = true;
 
 // create k8s Object
 const k = new K8SModel();
-k.name = "testk8s";
+k.name = "testk8s1";
 k.secret = "secret";
 k.network = n;
 k.masters = [master];
@@ -41,23 +41,35 @@ k.workers = [worker];
 k.metadata = "{'testk8s': true}";
 k.description = "test deploying k8s via ts grid3 client";
 k.ssh_key =
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWlguBuvfQikkRJZXkLPei7Scvo/OULUEvjWVR4tCZ5V85P2F4SsSghxpRGixCNc7pNtgvdwJegK06Tn7SkV2jYJ9kBJh8PA06CPSz1mnpco4cgktiWx/R8xBvLGlyO0BwUuD3/WFjrc6fzH9E7Bpkel/xTnacx14w1bZAC1R35hz7BaHu1WrXsfxEd0VH7gpMPoQ4+l+H38ULPTiC+JcOKJOqVafgcc0sU7otXbgCa1Frr4QE5bwiMYhOlsRfRv/hf08jYsVo+RUO3wD12ylLWR7a7sJDkBBwgir8SwAvtRlT6k9ew9cDMQ7H8iWNCOg2xqoTLpVag6RN9kGzA5LGL+qHEcBr6gd2taFEy9+mt+TWuKp6reUeJfTu9RD1UgB0HpcdgTHtoUTISW7Mz4KNkouci2DJFngDWrLRxRoz81ZwfI2hjFY0PYDzF471K7Nwwt3qKYF1Js9a6VO38tMxSU4mTO83bt+dUFozgpw2Y0KKJGHDwU66i2MvTPg3EGs= ayoub@ayoub-Inspiron-3576";
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCt1LYcIga3sgbip5ejiC6R7CCa34omOwUilR66ZEvUh/u4RpbZ9VjRryVHVDyYcd/qbUzpWMzqzFlfFmtVhPQ0yoGhxiv/owFwStqddKO2iNI7T3U2ytYLJqtPm0JFLB5n07XLyFRplq0W2/TjNrYl51DedDQqBJDq34lz6vTkECNmMKg9Ld0HpxnpHBLH0PsXMY+JMZ8keH9hLBK61Mx9cnNxcLV9N6oA6xRCtwqOdLAH08MMaItYcJ0UF/PDs1PusJvWkvsH5/olgayeAReI6JFGv/x4Eqq5vRJRQjkj9m+Q275gzf9Y/7M/VX7KOH7P9HmDbxwRtOq1F0bRutKF";
 
 async function main() {
     const grid3 = await getClient();
     // deploy k8s
-    const res = await grid3.k8s.deploy(k);
-    log(res);
-
-    // get the deployment
-    const l = await grid3.k8s.getObj(k.name);
-    log(l);
+    grid3.k8s.deploy(k)
+        .then(res => {
+            log(res);
+            // get the deployment
+            grid3.k8s.getObj(k.name)
+                .then(res_l => {
+                    log(res_l);
+                })
+                .catch(err => {
+                    throw err;
+                })
+                .finally(() => {
+                    grid3.disconnect();
+                })
+        })
+        .catch(err => {
+            grid3.disconnect();
+            throw err;
+        });
 
     // // delete
     // const d = await grid3.k8s.delete({ name: k.name });
     // log(d);
 
-    grid3.disconnect();
 }
 
 main();

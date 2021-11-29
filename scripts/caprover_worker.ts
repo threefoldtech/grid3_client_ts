0e40f543659edec6ae54833b697e48f2ea7275da
RafyAmgadBenjamin <rafy.amgadbenjamin@gmail.com>
2021-11-29 12:43:10 +0200

    Update caprover leader and worker script with latest caprover changes

--- scripts/caprover_worker.ts
+++ scripts/caprover_worker.ts
@@ -5,7 +5,7 @@ import { NetworkModel, MachineModel, MachinesModel, DiskModel } from "../src/mod
 import { getClient } from "./client_loader";
 // import {generateString} from "../src/helpers/utils";
 
-const CAPROVER_FLIST = "https://hub.grid.tf/samehabouelsaad.3bot/abouelsaad-caprover-tf_10.0.1_v1.0.flist";
+const CAPROVER_FLIST = "https://hub.grid.tf/tf-official-apps/tf-caprover-main.flist";
 // create network Object
 const n = new NetworkModel();
 n.name = "wedtest";
@@ -20,7 +20,7 @@ disk.mountpoint = "/testdisk"; //update this
 // create vm node Object
 const vm = new MachineModel();
 vm.name = "capworker1";
-vm.node_id = 17;
+vm.node_id = 14;
 vm.disks = [disk];
 vm.public_ip = true;
 vm.planetary = false;
@@ -31,15 +31,15 @@ vm.flist = CAPROVER_FLIST;
 vm.entrypoint = "/sbin/zinit init";
 vm.env = {
     PUBLIC_KEY:
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZ2f0jzr1xl54nWtSFZmyYTTiEjYHH/SkHI5fEvPHzQGzUeh7sP2z0mwGLcr5u/Y/hhKLfo4nULIPs9hZiLN5zZCUR9kCqZVr5mL+rTbmWyHVGsY4ozEZdxpi+pcVfLpyD3KTEnCVBMcB9SKHVcCMDL0NkVzxbf7rn/zvqCNOvvk75ofh9s29GvMtykkep6I+3jCvwRGJN4+ZVne0dz2fLFasZ/q/RPAKX+JQdTaP2yL4GKpFPtmo4iFLxSvMZTzSi887VsjukxMFQ/Plv+5c1rdI6nhDQM8kWQxs6UkM5FBqqAwxMra8V2HXqtG3KyCRdIyzrjbEeaGZilNLVv108Oq82/JhJLfWQcE6WmPfjm5gAMuiZYAjyNOFaCV42q3bZqtbqjlMz+qcU4tzlla5MMx7Kj4TILYZqhmzYsX7Xb/CnGS2XqhlKMZrpHY12V00wVWH+lWKi1JWIR7cyhri89VUhftTyUUeYEyTL9/8HoiSeqtjPIsxrkGyybT6b2S8= rafy@rafy-Inspiron-3576",
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDF7MKO2kjhnc3K02hsvJrMofIc8aploPsbPXzPZgeegd4sVJiGzdnTfiTjNUl7mdvct2FpoBBWQd9SeiLAW592CHMP9pOXO2CzOi/xNBrar7TnBc6nNnPjbd9bQEgLK9b2LQLCzLmZQmTWJolPETrkWcCLt4fmTchxHKROoRG6TOfAW2rieJSY8yj1+xtvjIHtFceD7vNByI62kOTzdlzOVmiGZ+9gkBJHSRTZkWniaACg0Mt3R9Xq6q6XHpIPTGqSOXeundPpaw+z+PUBc42Aa+LIgV2aoZP00yokx7WCttG3tS+xLv/6pEmIst5b/m4WNMkBx9fkGEIM4eaAH2mFXNc0bQNQJVqXEQSi7DWecWzNkHKDqRN6HoN/BAUF+clKW3fkvWme8iE8XK9xRCiNc8G3sQh2xIajKj0UbKn88k1fYMughJye93Q82mKXFw5QU3S0GzH/YoOEWhXUO8N76zZz+jyjYg9VBy/AU8lm1UJxAjiANIydkXjBFFofLh8= rafy@rafy-Inspiron-3576",
     SWM_NODE_MODE: "worker",
-    SWMTKN: "SWMTKN-1-0892ds1ney7pa0hymi3qwph7why1d9r3z6bvwtin51r14hcz3t-cjsephnu4f2ezfpdd6svnnbq7",
-    LEADER_PUBLIC_IP: "185.206.122.44",
+    SWMTKN: "SWMTKN-1-1eikxeyat4br9t4la1dnln11l1tvlnrngzwh5iq68m2vn7edi1-6lc6xtw3pzd99lrowyuayr5yv",
+    LEADER_PUBLIC_IP: "185.206.122.157",
 };
 
 // create VMs Object
 const vms = new MachinesModel();
-vms.name = "newVMS5";
+vms.name = "newVMS6";
 vms.network = n;
 vms.machines = [vm];
 vms.metadata = "{'testVMs': true}";

432a567b09661aee723d62bece8458ca8e7c52a3
RafyAmgadBenjamin <46320202+RafyAmgadBenjamin@users.noreply.github.com>
2021-11-29 13:56:01 +0200

    Development caproverscript (#115)
    * Working on caprover scripts for leader and worker nodes
    
    * Update caprover leader and worker script with latest caprover changes
    
    * adding comment on the worker node to make it clear that the env vars depend on the leader node

---
+++ scripts/caprover_leader.ts
@@ -0,0 +1,71 @@
+import "reflect-metadata";
+
+import { log } from "./utils";
+import { NetworkModel, MachineModel, MachinesModel, DiskModel } from "../src/modules/models";
+import { getClient } from "./client_loader";
+// import {generateString} from "../src/helpers/utils";
+
+const CAPROVER_FLIST = "https://hub.grid.tf/tf-official-apps/tf-caprover-main.flist";
+// create network Object
+const n = new NetworkModel();
+n.name = "wedtest";
+n.ip_range = "10.249.0.0/16";
+
+// create disk Object
+const disk = new DiskModel();
+disk.name = "wedDisk";
+disk.size = 100;
+disk.mountpoint = "/testdisk";
+
+// create vm node Object
+const vm = new MachineModel();
+vm.name = "testvm";
+vm.node_id = 14;
+vm.disks = [disk];
+vm.public_ip = true;
+vm.planetary = false;
+vm.cpu = 4;
+vm.memory = 1024 * 4;
+vm.rootfs_size = 10;
+vm.flist = CAPROVER_FLIST;
+vm.entrypoint = "/sbin/zinit init";
+vm.env = {
+    PUBLIC_KEY:
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDF7MKO2kjhnc3K02hsvJrMofIc8aploPsbPXzPZgeegd4sVJiGzdnTfiTjNUl7mdvct2FpoBBWQd9SeiLAW592CHMP9pOXO2CzOi/xNBrar7TnBc6nNnPjbd9bQEgLK9b2LQLCzLmZQmTWJolPETrkWcCLt4fmTchxHKROoRG6TOfAW2rieJSY8yj1+xtvjIHtFceD7vNByI62kOTzdlzOVmiGZ+9gkBJHSRTZkWniaACg0Mt3R9Xq6q6XHpIPTGqSOXeundPpaw+z+PUBc42Aa+LIgV2aoZP00yokx7WCttG3tS+xLv/6pEmIst5b/m4WNMkBx9fkGEIM4eaAH2mFXNc0bQNQJVqXEQSi7DWecWzNkHKDqRN6HoN/BAUF+clKW3fkvWme8iE8XK9xRCiNc8G3sQh2xIajKj0UbKn88k1fYMughJye93Q82mKXFw5QU3S0GzH/YoOEWhXUO8N76zZz+jyjYg9VBy/AU8lm1UJxAjiANIydkXjBFFofLh8= rafy@rafy-Inspiron-3576",
+    SWM_NODE_MODE: "leader",
+    CAPROVER_ROOT_DOMAIN: "rafy.grid.tf",
+    DEFAULT_PASSWORD: "captain42"
+};
+
+// create VMs Object
+const vms = new MachinesModel();
+vms.name = "newVMS5";
+vms.network = n;
+vms.machines = [vm];
+vms.metadata = "{'testVMs': true}";
+vms.description = "caprover leader machine/node";
+
+async function main() {
+    const grid3 = await getClient();
+
+    // deploy vms
+    try {
+        const res = await grid3.machines.deploy(vms);
+        log(res);
+
+        // get the deployment
+        const l = await grid3.machines.getObj(vms.name);
+        log(l);
+
+        // // delete
+        // const d = await grid3.machines.delete({ name: vms.name });
+        // log(d);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    } finally {
+        grid3.disconnect();
+    }
+}
+
+main();

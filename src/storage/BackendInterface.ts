3fa05edc547a67b2321ff4d8811358ee25305872
Mahmoud Ayoub <36021484+dmahmouali@users.noreply.github.com>
2021-12-20 20:05:38 +0200

    Update src/storage/BackendInterface.ts
    Co-authored-by: AhmedHanafy725 <41957921+AhmedHanafy725@users.noreply.github.com>

--- src/storage/BackendInterface.ts
+++ src/storage/BackendInterface.ts
@@ -1,8 +1,8 @@
 interface BackendInterface {
-    get(key: string): Promise<any>;
-    set(key: string, value: any): Promise<any>;
+    get(key: string): Promise<string>;
+    set(key: string, value: string): Promise<any>;
     remove(key: string): Promise<any>;
-    list(key: string): Promise<any>;
+    list(key: string): Promise<string[]>;
 }
 
 export default BackendInterface;

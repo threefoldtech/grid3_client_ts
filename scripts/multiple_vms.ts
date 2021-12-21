0f842e4fde2c2626791beee2524512bea6b1d602
ahmedhanafy725 <hanafya@codescalers.com>
2021-12-21 15:12:45 +0200

    Update scripts to use ssh key for config.json

--- scripts/multiple_vms.ts
+++ scripts/multiple_vms.ts
@@ -26,8 +26,7 @@ vm1.rootfs_size = 1;
 vm1.flist = "https://hub.grid.tf/tf-official-apps/base:latest.flist";
 vm1.entrypoint = "/sbin/zinit init";
 vm1.env = {
-    SSH_KEY:
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCt1LYcIga3sgbip5ejiC6R7CCa34omOwUilR66ZEvUh/u4RpbZ9VjRryVHVDyYcd/qbUzpWMzqzFlfFmtVhPQ0yoGhxiv/owFwStqddKO2iNI7T3U2ytYLJqtPm0JFLB5n07XLyFRplq0W2/TjNrYl51DedDQqBJDq34lz6vTkECNmMKg9Ld0HpxnpHBLH0PsXMY+JMZ8keH9hLBK61Mx9cnNxcLV9N6oA6xRCtwqOdLAH08MMaItYcJ0UF/PDs1PusJvWkvsH5/olgayeAReI6JFGv/x4Eqq5vRJRQjkj9m+Q275gzf9Y/7M/VX7KOH7P9HmDbxwRtOq1F0bRutKF",
+    SSH_KEY: config.ssh_key,
 };
 
 // create disk Object

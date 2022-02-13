6ae6ba5809b235b8d83002261c71824c160962dc
ahmedhanafy725 <hanafya@codescalers.com>
2022-02-13 14:12:09 +0200

    Add better error for the rmb request

--- src/clients/rmb/client.ts
+++ src/clients/rmb/client.ts
@@ -1,5 +1,7 @@
 import { MessageBusClientInterface } from "ts-rmb-client-base";
 
+import { events } from "../../helpers/events";
+
 class RMB {
     client: MessageBusClientInterface;
     constructor(rmbClient: MessageBusClientInterface) {
@@ -13,13 +15,21 @@ class RMB {
             const message = await this.client.send(msg, payload);
             result = await this.client.read(message);
 
+            if (!result[0]) {
+                throw Error(`Failed to get the response from twinId: ${destTwinIds} for command: ${cmd}`);
+            }
+
             if (result[0].err) {
-                throw Error(String(result[0].err));
+                throw Error(
+                    `Failed to apply command: ${cmd} on twinId: ${destTwinIds} due to ${String(result[0].err)}`,
+                );
             }
         } catch (e) {
-            throw Error(
-                `Failed to send request to ${destTwinIds} with command: ${cmd}, payload: ${payload} after ${retires} retries due to ${e}`,
+            events.emit(
+                "logs",
+                `Failed to send request to twinId ${destTwinIds} with command: ${cmd}, payload: ${payload} after ${retires} retries due to ${e}`,
             );
+            throw Error(e);
         }
         if (result[0].dat) {
             return JSON.parse(String(result[0].dat));

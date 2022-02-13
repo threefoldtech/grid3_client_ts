dcccc6a8bf576a9c7a33c58eec24386907bbb8b9
ahmedhanafy725 <hanafya@codescalers.com>
2022-02-13 14:13:56 +0200

    Add better error for connecting the client for non registered user

--- src/client.ts
+++ src/client.ts
@@ -41,7 +41,12 @@ class GridClient {
             process.on("SIGUSR1", this.disconnectAndExit);
             process.on("SIGUSR2", this.disconnectAndExit);
         } else window.onbeforeunload = this.disconnect;
-        this.twinId = await tfclient.twins.getMyTwinId();
+        try {
+            this.twinId = await tfclient.twins.getMyTwinId();
+        } catch (e) {
+            console.log(e);
+            throw Error(`Couldn't find a user for the provided mnemonic on ${this.network} network.`);
+        }
         this._connect();
     }
     _connect(): void {
